e342789622dac18cf1251c8afbffdf0b3c2f55f5
Andrew Morrison <morro141@gmail.com>
2024-11-30 17:45:18 -0500

    update init.lua

--- lua/line_notes/init.lua
+++ lua/line_notes/init.lua
@@ -2,11 +2,11 @@ local M = {}
 
 function M.setup(opts)
 	opts = opts or {}
-	vim.api.nvim_set_keymap("n", "<leader>am", ":lua require('line_notes.marks').add_mark()<CR>",
+	vim.api.nvim_set_keymap("n", opts.add_mark_key or "<leader>am", ":lua require('line_notes.marks').add_mark()<CR>",
 		{ noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "<leader>lm", ":lua require('line_notes.telescope_picker').open_marks_picker()<CR>",
-		{ noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "<leader>dm", ":lua require('line_notes.marks').delete_mark()<CR>",
+	vim.api.nvim_set_keymap("n", opts.list_marks_key or "<leader>lm",
+		":lua require('line_notes.telescope_picker').open_marks_picker()<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", opts.delete_mark_key or "<leader>dm", ":lua require('line_notes.marks').delete_mark()<CR>",
 		{ noremap = true, silent = true })
 end
 
