1440c40361783d243aff5f41840c277581314bd4
Andrew Morrison <morro141@gmail.com>
2024-11-30 17:35:04 -0500

    properly referencing files

--- plugin/line_notes.lua
+++ plugin/line_notes.lua
@@ -1,13 +1,1 @@
-local M
-
-function M.setup(opts)
-	opts = opts or {}
-	vim.api.nvim_set_keymap("n", "<leader>am", ":lua require('line_notes.marks').add_mark()<CR>",
-		{ noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "<leader>lm", ":lua require('line_notes.telescope_picker').open_marks_picker()<CR>",
-		{ noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "<leader>dm", ":lua require('line_notes.marks').delete_mark()<CR>",
-		{ noremap = true, silent = true })
-end
-
-return M
+require('line_notes').setup()
